7502c053a61820ba969cd4c13e311481a4687900
GabrielAdans <adans.gabriel@gmail.com>
2024-01-05 15:43:00 -0300

    fix: construção do componente Card com a implantação do PropTypes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,28 @@
 import "../style/css/componentsStyle/ourservice.css";
 import "../style/css/structure-global.css";
+import { ReactPropTypes } from "react";
+import { FaTimes } from "react-icons/fa";
 
 const Card = ({ titulo, conteudo }) => {
   return (
     <div className="card">
+      <FaTimes />
       <h2>{titulo}</h2>
       <p>{conteudo}</p>
+      <a
+        href="https://www.google.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Saiba mais
+      </a>
     </div>
   );
+};
+
+Card.prototype = {
+  titulo: ReactPropTypes.string.isRequired,
+  conteudo: ReactPropTypes.string.isRequired,
 };
 
 export default Card;
