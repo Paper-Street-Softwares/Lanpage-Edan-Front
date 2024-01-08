1a88e27123ff2c58fd331a29dc51cb507f70f2a6
GabrielAdans <adans.gabriel@gmail.com>
2024-01-08 20:36:43 -0300

    feat: criação da sessão links úteis e enquadramento à régua imaginária

--- src/components/UsefullLinks.js
+++ src/components/UsefullLinks.js
@@ -1,11 +1,85 @@
+import "../style/css/componentsStyle/usefulllinks.css";
+import styled from "styled-components";
+import React from "react";
+
+const ContainerUsefullLinks = styled.div`
+  ${(props) =>
+    props.theme.containerUsefullLinks}/* Usando os estilos do arquivo CSS */
+`;
+
+const GridItem = styled.div`
+  ${(props) => props.theme.gridItem}/* Usando os estilos do arquivo CSS */
+`;
+
 function UsefullLinks() {
   return (
-    <div>
-      <h1>Quadro com sites úteis e Certidões Negativas</h1>
-      <h3>
-        Cartão CNPJ / Insc Estadual / Opção simples / CND-PJ / CND-PF / CRF /
-        CNDT / Sefaz
-      </h3>
+    <div className="wrapper-UsefullLinks">
+      <div className="titleCenter-UsefullLinks">
+        <h1>LINKS ÚTEIS</h1>
+      </div>
+      <ContainerUsefullLinks className="container-usefull-links">
+        <GridItem className="grid-item">
+          <h1>CARTÃO CNPJ</h1>
+          <p>
+            esqueça tudo esqueça tudo esqueça tudo esqueça tudo esqueça tudo
+          </p>
+          <a href="/">ESQUEÇA TUDO</a>
+        </GridItem>
+
+        <GridItem className="grid-item">
+          <h1>INSC ESTADUAL</h1>
+          <p>
+            esqueça tudo esqueça tudo esqueça tudo esqueça tudo esqueça tudo
+          </p>
+          <a href="/">ESQUEÇA TUDO</a>
+        </GridItem>
+        <GridItem className="grid-item">
+          <h1>OPÇÃO SIMPLES</h1>
+          <p>
+            esqueça tudo esqueça tudo esqueça tudo esqueça tudo esqueça tudo
+          </p>
+          <a href="/">ESQUEÇA TUDO</a>
+        </GridItem>
+      </ContainerUsefullLinks>
+      <ContainerUsefullLinks className="container-usefull-links">
+        <GridItem className="grid-item">
+          <h1>CND-PJ</h1>
+          <p>
+            esqueça tudo esqueça tudo esqueça tudo esqueça tudo esqueça tudo
+          </p>
+          <a href="/">ESQUEÇA TUDO</a>
+        </GridItem>
+        <GridItem className="grid-item">
+          <h1>CND-PF</h1>
+          <p>
+            esqueça tudo esqueça tudo esqueça tudo esqueça tudo esqueça tudo
+          </p>
+          <a href="/">ESQUEÇA TUDO</a>
+        </GridItem>
+        <GridItem className="grid-item">
+          <h1>CRF</h1>
+          <p>
+            esqueça tudo esqueça tudo esqueça tudo esqueça tudo esqueça tudo
+          </p>
+          <a href="/">ESQUEÇA TUDO</a>
+        </GridItem>
+      </ContainerUsefullLinks>
+      <ContainerUsefullLinks className="container-usefull-links">
+        <GridItem className="grid-item">
+          <h1>CNDT</h1>
+          <p>
+            esqueça tudo esqueça tudo esqueça tudo esqueça tudo esqueça tudo
+          </p>
+          <a href="/">ESQUEÇA TUDO</a>
+        </GridItem>
+        <GridItem className="grid-item">
+          <h1>SEFAZ</h1>
+          <p>
+            esqueça tudo esqueça tudo esqueça tudo esqueça tudo esqueça tudo
+          </p>
+          <a href="/">ESQUEÇA TUDO</a>
+        </GridItem>
+      </ContainerUsefullLinks>
     </div>
   );
 }
