592f22f4bb9565551d748f79a386a9835cbd10c8
GabrielAdans <adans.gabriel@gmail.com>
2024-01-09 13:56:08 -0300

    feat: construção do awards e footer

--- src/components/FooterPage.js
+++ src/components/FooterPage.js
@@ -1,6 +1,13 @@
 import "../style/css/componentsStyle/footerpage.css";
 import Logo from "../style/assets/images/EdanLogo.png";
 import { FaHome, FaPhoneAlt, FaRegClock } from "react-icons/fa";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaLinkedinIn,
+  FaInstagram,
+  FaSignal,
+} from "react-icons/fa";
 
 function FooterPage() {
   return (
@@ -13,7 +20,7 @@ function FooterPage() {
           <div className="infos-FooterPage">
             <div className="address-FooterPage">
               <FaHome />
-              <p>Endereço esqueça tudo</p>
+              <p>esqueça tudo</p>
             </div>
             <div className="phone-FooterPage">
               <FaPhoneAlt />
@@ -25,8 +32,67 @@ function FooterPage() {
             </div>
           </div>
         </div>
-        <div className="footerItem-FooterPage">CONTEUDO 2</div>
-        <div className="footerItem-FooterPage">CONTEUDO 3</div>
+        <div className="footerItem-FooterPage">
+          <div className="titleMiddle-FooterPage">
+            <h1>SOBRE</h1>
+          </div>
+          <br />
+          <div className="span-FooterPage">
+            <span>
+              esqueça tudo esqueça tudo esqueça tudo esqueça tudo esqueça tudo
+              esqueça tudo esqueça tudo esqueça tudo esqueça tudo esqueça tudo
+            </span>
+          </div>
+          <br />
+          <div className="icons-FooterPage">
+            <FaFacebook />
+            <FaTwitter />
+            <FaLinkedinIn />
+            <FaInstagram />
+            <FaSignal />
+          </div>
+        </div>
+        <div className="footerItem-FooterPage">
+          <div className="titleRighter-FooterPage">
+            <h1>NAVEGAÇÃO</h1>
+          </div>
+          <br />
+          <div className="links-FooterPage">
+            <ul className="list-FooterPage">
+              <div className="columns-FooterPage">
+                <div className="col-1">
+                  <li className="item-FooterPage">
+                    <button href="/">
+                      <a>INÍCIO</a>
+                    </button>
+                  </li>
+                  <li className="item-FooterPage">
+                    <button href="/src/components/About.js">
+                      <a>SOBRE NÓS</a>
+                    </button>
+                  </li>
+                  <li className="item-FooterPage">
+                    <button href="/src/components/OurService.js">
+                      <a>SERVIÇOS</a>
+                    </button>
+                  </li>
+                </div>
+                <div className="col-2">
+                  <li className="item-FooterPage">
+                    <button href="/src/components/UsefullLinks.js">
+                      <a>LINKS</a>
+                    </button>
+                  </li>
+                  <li className="item-FooterPage">
+                    <button href="/src/components/About.js">
+                      <a>CONTATO</a>
+                    </button>
+                  </li>
+                </div>
+              </div>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
