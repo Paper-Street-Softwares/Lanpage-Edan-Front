8d70daceda5ef729441df15243b520c00d2583ce
GabrielAdans <adans.gabriel@gmail.com>
2024-01-09 14:51:42 -0300

    feat:bugfix sonar

--- src/components/ButtonSlide.js
+++ src/components/ButtonSlide.js
@@ -1,17 +1,17 @@
-// import { MdArrowForwardIos } from "react-icons/md";
-import "../style/css/componentsStyle/buttonslide.css";
+// // import { MdArrowForwardIos } from "react-icons/md";
+// import "../style/css/componentsStyle/buttonslide.css";
 
-function ButtonSlide({ onNext, onPrev }) {
-  return (
-    <div className="btns-slide">
-      {/* <button id="prev-button" onClick={onPrev}>
-        <MdArrowForwardIos />
-      </button>
-      <button id="next-button" onClick={onNext}>
-        <MdArrowForwardIos />
-      </button> */}
-    </div>
-  );
-}
+// function ButtonSlide({ onNext, onPrev }) {
+//   return (
+//     <div className="btns-slide">
+//       {/* <button id="prev-button" onClick={onPrev}>
+//         <MdArrowForwardIos />
+//       </button>
+//       <button id="next-button" onClick={onNext}>
+//         <MdArrowForwardIos />
+//       </button> */}
+//     </div>
+//   );
+// }
 
-export default ButtonSlide;
+// export default ButtonSlide;
