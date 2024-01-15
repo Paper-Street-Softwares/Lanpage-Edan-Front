c1f5f67e2219c4b4cf72f839a4aa6c5ea7618602
GabrielAdans <adans.gabriel@gmail.com>
2024-01-15 10:07:12 -0300

    feat: construção da navbar com floating para mobiles

--- src/components/HeaderSection/Navbar.js
+++ src/components/HeaderSection/Navbar.js
@@ -2,52 +2,62 @@ import "../../style/css/componentsStyle/navbar.css";
 import Logo from "../../style/assets/images/Logo.png";
 import { Component } from "react";
 
-class Navbar extends Component() {
-  state={clicked: false}
-  return (
-    <div className="wrapper-Navbar">
-      <div className="container-Navbar">
-        <div className="logo-Navbar">
-          <img src={Logo} alt="Logo da Empresa" />
-        </div>
-        <div className="buttonsCascade-Navbar">
-          <ul className="list">
-            <li className="item">
-              <button href="/">
-                <a>INÍCIO</a>
-              </button>
-            </li>
-            <li className="item">
-              <button href="/src/components/About.js">
-                <a>SOBRE NÓS</a>
-              </button>
-            </li>
-            <li className="item">
-              <button href="/src/components/OurService.js">
-                <a>SERVIÇOS</a>
-              </button>
-            </li>
-            <li className="item">
-              <button href="/src/components/UsefullLinks.js">
-                <a>LINKS</a>
-              </button>
-            </li>
-            <li className="item">
-              <button href="/src/components/About.js">
-                <a>CONTATO</a>
-              </button>
-            </li>
-          </ul>
-        </div>
-        <div id="mobile">
-          <i
-            id="bars"
-            className={this.state.clicked ? "fa fas-times" : "fa fas-bars"}
-          ></i>
+class Navbar extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked });
+  };
+  render() {
+    return (
+      <div className="wrapper-Navbar">
+        <div className="container-Navbar">
+          <div className="logo-Navbar">
+            <img src={Logo} alt="Logo da Empresa" />
+          </div>
+          <div className="buttonsCascade-Navbar">
+            <ul
+              id="list-Navbar"
+              className={
+                this.state.clicked ? "#list-Navbar active" : "#list-Navbar"
+              }
+            >
+              <li className="item">
+                <button>
+                  <a href="/">INÍCIO</a>
+                </button>
+              </li>
+              <li className="item">
+                <button>
+                  <a href="/src/components/About.js">SOBRE NÓS</a>
+                </button>
+              </li>
+              <li className="item">
+                <button>
+                  <a href="/src/components/OurService.js">SERVIÇOS</a>
+                </button>
+              </li>
+              <li className="item">
+                <button href="/src/components/UsefullLinks.js">
+                  <a href="/src/components/UsefullLinks.js">LINKS</a>
+                </button>
+              </li>
+              <li className="item">
+                <button href="/src/components/About.js">
+                  <a href="/src/components/About.js">CONTATO</a>
+                </button>
+              </li>
+            </ul>
+          </div>
+          <div id="mobile" onClick={this.handleClick}>
+            <i
+              id="bars"
+              className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+            ></i>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Navbar;
