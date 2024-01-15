b4e94daa3486d6b35bdb21d50ea3185cc3ca8ddd
Edison Matos <edison.matos@live.com>
2024-01-15 09:33:16 -0300

    feat: Teste de erro 2

--- arquivoComErroTeste.js
+++ arquivoComErroTeste.js
@@ -1,10 +1,15 @@
-arquivo com erro pra test ssonar
+arquivo com erro pra test ssonasdsaar
 
 // código comentado
 // código comentado
 // código comentado
 // código comentado
 // código comentado
+const a = 10
+
+const a = 5
+
 // código comentado
+dsad
 // código comentado
 
