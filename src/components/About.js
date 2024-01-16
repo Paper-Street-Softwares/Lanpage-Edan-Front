9337000445f2d2a6272cb2150dadddbba619755e
GabrielAdans <adans.gabriel@gmail.com>
2024-01-16 08:48:35 -0300

    feat: Criação de componente FAQ e rework do ContactUs

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import "../style/css/componentsStyle/about.css";
-import ContactForm from "../components/ContactSection/ContactForm";
-import ContactInfo from "../components/ContactSection/ContactInfo";
+import CustomizedAccordions from "./ContactSection/CustomizedAccordions";
+import InputWithIcon from "./ContactSection/InputWithIcon";
 
 function About() {
   return (
@@ -8,37 +8,15 @@ function About() {
       <div className="container-About">
         <div className="sectionLeft">
           <div className="titleAbout">
-            <h1>SOBRE A EMPRESA</h1>
-
-            <h4>
-              Somos uma empresa líder em consultoria especializada em
-              investimentos financeiros
-            </h4>
-            <br />
+            <h1>PERGUNTAS FREQUENTES</h1>
+            <CustomizedAccordions />
           </div>
-          <p>
-            Com uma reputação consolidada, nossa empresa se destaca como líder
-            em consultoria especializada em investimentos financeiros. Contamos
-            com uma equipe altamente qualificada de especialistas financeiros
-            dedicados a oferecer serviços de excelência aos nossos clientes.
-            Nossa abordagem centrada no cliente visa compreender as metas
-            financeiras individuais e criar estratégias personalizadas para
-            atendê-las. Utilizamos análises de mercado avançadas e informações
-            atualizadas para orientar nossas recomendações, sempre priorizando a
-            maximização de retornos e a gestão eficaz de riscos. Com um
-            compromisso inabalável com a transparência e a ética, estamos
-            prontos para liderar nossos clientes em direção ao sucesso
-            financeiro duradouro. ‌
-          </p>
-          <br />
         </div>
         <div className="sectionRight">
           <div className="titleAbout">
-            <h1>Faça um orçamento grátis</h1>
+            <h1>CONTATO</h1>
           </div>
-          <ContactForm />
-          <br />
-          <ContactInfo />
+          <InputWithIcon />
         </div>
       </div>
     </div>
