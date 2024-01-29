020393a8f7126fd21147b135700d5c9796b8bfa7
Edison Matos <edison.matos@live.com>
2024-01-29 17:43:13 -0300

    feat: Cria componente SeactionHeader para reaproveitar nos cabeçalhos das seções

---
+++ src/components/SectionComponents/SectionHeader.jsx
@@ -0,0 +1,20 @@
+export default function SectionHeader(props) {
+  const sectionHeaderTitleFirst = props.sectionHeaderTitleFirst;
+  const sectionHeaderTitleSecond = props.sectionHeaderTitleSecond;
+  const sectionHeaderDescription = props.sectionHeaderDescription;
+
+  return (
+    <div className="flex flex-col pt-3 pb-3">
+      <div className="w-[79%] flex flex-col justify-center font-poppins text-2xl text-center font-bold ">
+        {sectionHeaderTitleFirst}
+      </div>
+      <div className="w-[79%] flex flex-col justify-center font-poppins text-green text-2xl text-center font-bold ">
+        {sectionHeaderTitleSecond}
+      </div>
+      <div className="w-full h-px bg-neutral-200 mt-6 mb-6"></div>
+      <div className="w-[79%] text-neutral-500 font-poppins text-sm text-center">
+        {sectionHeaderDescription}
+      </div>
+    </div>
+  );
+}
--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,7 +1,13 @@
+import SectionHeader from "./SectionComponents/SectionHeader";
+
 export default function ServicesSection() {
   return (
     <div className="content">
-      <h1>Content</h1>
+      <SectionHeader
+        sectionHeaderTitleFirst="Veja o que nossos clientes têm a dizer"
+        sectionHeaderTitleSecond="sobre o nosso trabalho"
+        sectionHeaderDescription="Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney."
+      />
     </div>
   );
 }
