cd9a5ea882faa2f05b6cadfca68bac7bcd362ee3
Edison Matos <edison.matos@live.com>
2024-01-29 18:51:42 -0300

    feat: Cria componente SeactionHeader para reaproveitar nos cabeçalhos das seções

--- src/components/SectionComponents/ImgAndTitleCard.jsx
+++ src/components/SectionComponents/ImgAndTitleCard.jsx
@@ -4,8 +4,15 @@ export default function ImgAndTitleCard(props) {
   const title = props.title;
 
   return (
-    <div className="bg-blue-100 flex flex-col justify-center items-center w-36 h-44">
-      <img src={img} alt={imgDescription} className="bg-yellow-100 h-14"></img>
+    <div className="bg-blue-100 flex flex-col justify-around items-center w-36 h-44">
+      <div>
+        <img
+          src={img}
+          alt={imgDescription}
+          className="bg-yellow-100 h-14"
+        ></img>
+      </div>
+
       <div className="bg-red-100 font-poppins text-center font-bold text-sm">
         {title}
       </div>
