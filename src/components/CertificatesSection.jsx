1c01aea5c8de325dcca1e8db8b6ec300ecbfeb49
Edison Matos <edison.matos@live.com>
2024-01-30 11:28:28 -0300

    feat: Insere SectionContent e prepara receber cards

--- src/components/CertificatesSection.jsx
+++ src/components/CertificatesSection.jsx
@@ -1,3 +1,4 @@
+import SectionContent from "./SectionComponents/SectionContent";
 import SectionHeader from "./SectionComponents/SectionHeader";
 
 export default function CertificatesSection() {
@@ -8,6 +9,7 @@ export default function CertificatesSection() {
         sectionHeaderTitleSecond="sua empresa precisa"
         sectionHeaderDescription="A Edan Contabilidade simplifica seu dia a dia com links diretos em nosso site para retirada ágil das certidões negativas essenciais. Praticidade e eficiência para o sucesso contínuo do seu negócio."
       />
+      <SectionContent>{[]}</SectionContent>
     </div>
   );
 }
