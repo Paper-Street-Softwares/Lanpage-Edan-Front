2ca3387f469878614f7c9f0abba25ad4eb57312d
Edison Matos <edison.matos@live.com>
2024-01-30 17:48:08 -0300

    feat: Cria a seção de conteudo em Diferenciais

--- src/components/DifferentialSection.jsx
+++ src/components/DifferentialSection.jsx
@@ -1,4 +1,5 @@
 import SectionHeader from "../components/SectionComponents/SectionHeader";
+import SectionContent from "./SectionComponents/SectionContent";
 
 export default function DifferentialSection() {
   return (
@@ -8,6 +9,7 @@ export default function DifferentialSection() {
         sectionHeaderTitleSecond="sua empresa precisa"
         sectionHeaderDescription="Não apenas contabilizamos números, construímos caminhos para o seu sucesso financeiro. Nossos diferenciais vão além da simples prestação de serviços contábeis."
       />
+      <SectionContent>{[]}</SectionContent>
     </div>
   );
 }
