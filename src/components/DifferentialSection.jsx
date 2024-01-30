1e0a294b60d8abd4354fa19e423e1b9825ed0a8b
Edison Matos <edison.matos@live.com>
2024-01-30 18:07:38 -0300

    feat: Adiciona cards com icones em Diferenciais

--- src/components/DifferentialSection.jsx
+++ src/components/DifferentialSection.jsx
@@ -1,6 +1,14 @@
 import SectionHeader from "../components/SectionComponents/SectionHeader";
 import ImgAndTitleCard from "./SectionComponents/ImgAndTitleCard";
 import SectionContent from "./SectionComponents/SectionContent";
+import imgPortal from "../style/assets/icons/diferenciais/portalDoCliente.png";
+import imgExperiencia from "../style/assets/icons/diferenciais/experiencia.png";
+import imgAtendimento from "../style/assets/icons/diferenciais/atendimento.png";
+import imgTomadaDecisoes from "../style/assets/icons/diferenciais/tomadaDeDecisoes.png";
+import imgExpertise from "../style/assets/icons/diferenciais/expertise.png";
+import imgTecnologia from "../style/assets/icons/diferenciais/tecnologia.png";
+import imgSegurança from "../style/assets/icons/diferenciais/seguranca.png";
+import imgConfianca from "../style/assets/icons/diferenciais/confianca.png";
 
 export default function DifferentialSection() {
   return (
@@ -14,9 +22,51 @@ export default function DifferentialSection() {
         {[
           <ImgAndTitleCard
             key={1}
-            img={""}
-            imgDescription="Ícone "
-            title="Título"
+            img={imgPortal}
+            imgDescription="Ícone Portal do Cliente"
+            title="Portal do Cliente"
+          />,
+          <ImgAndTitleCard
+            key={2}
+            img={imgExperiencia}
+            imgDescription="Ícone Experiência"
+            title="Experiência e Credibilidade"
+          />,
+          <ImgAndTitleCard
+            key={3}
+            img={imgAtendimento}
+            imgDescription="Ícone Atendimento"
+            title="Atendimento Qualificado"
+          />,
+          <ImgAndTitleCard
+            key={4}
+            img={imgTomadaDecisoes}
+            imgDescription="Ícone Tomada de Decisões"
+            title="Ajuda na tomada de decisões"
+          />,
+          <ImgAndTitleCard
+            key={5}
+            img={imgExpertise}
+            imgDescription="Ícone Expertise"
+            title="Expertise na área"
+          />,
+          <ImgAndTitleCard
+            key={6}
+            img={imgTecnologia}
+            imgDescription="Ícone Tecnologia"
+            title="Tecnologia e Otimização"
+          />,
+          <ImgAndTitleCard
+            key={7}
+            img={imgSegurança}
+            imgDescription="Ícone Segurança"
+            title="Segurança"
+          />,
+          <ImgAndTitleCard
+            key={8}
+            img={imgConfianca}
+            imgDescription="Ícone Confiança"
+            title="Confiança"
           />,
         ]}
       </SectionContent>
