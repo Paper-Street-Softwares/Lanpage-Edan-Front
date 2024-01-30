6882e6bc9a2413a66e720474e212fe19c2d670dd
Edison Matos <edison.matos@live.com>
2024-01-30 11:23:15 -0300

    feat: Insere a SectionHeader com as infos

--- src/components/CertificatesSection.jsx
+++ src/components/CertificatesSection.jsx
@@ -1,7 +1,13 @@
+import SectionHeader from "./SectionComponents/SectionHeader";
+
 export default function CertificatesSection() {
   return (
     <div className="content">
-      <h1>Seção Certidão Negativa</h1>
+      <SectionHeader
+        sectionHeaderTitleFirst="Todas as certidões que"
+        sectionHeaderTitleSecond="sua empresa precisa"
+        sectionHeaderDescription="A Edan Contabilidade simplifica seu dia a dia com links diretos em nosso site para retirada ágil das certidões negativas essenciais. Praticidade e eficiência para o sucesso contínuo do seu negócio."
+      />
     </div>
   );
 }
