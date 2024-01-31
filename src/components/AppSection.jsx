00dbb3ecb01594f2f76318a8021e12690863beeb
Edison Matos <edison.matos@live.com>
2024-01-31 12:01:51 -0300

    feat: Ajustar botões da seção Aplicativo

--- src/components/AppSection.jsx
+++ src/components/AppSection.jsx
@@ -1,5 +1,8 @@
 import SectionContent from "../components/SectionComponents/SectionContent";
 import imgAppScreen from "../style/assets/images/aplicativo/appScreens.png";
+import imgAppUtilities from "../style/assets/images/aplicativo/appUtilities.png";
+import imgAppStore from "../style/assets/images/aplicativo/appStore.png";
+import imgPlayStore from "../style/assets/images/aplicativo/playStore.png";
 
 export default function AppSection() {
   return (
@@ -7,23 +10,23 @@ export default function AppSection() {
       <div className="feature">
         <SectionContent>
           <div className="bg-red-100 flex flex-col justify-around w-128">
-            <div className="bg-yellow-100 flex flex-row justify-center font-poppins font-bold text-2xl text-darkgreen text-center">
+            <div className="bg-yellow-100 flex flex-row justify-center font-poppins font-bold text-2xl text-mediumgreen text-center">
               Baixe nosso aplicativo e tenha inúmeras vantagens
             </div>
             <div className="bg-green-100 flex flex-row justify-center">
               <img src={imgAppScreen} />
             </div>
           </div>
-          <div className="bg-green-100 flex flex-col justify-around w-128 h-40">
-            <div className="bg-purple-100 flex flex-row justify-center">
+          <div className="bg-mediumgreen flex flex-col justify-around w-128">
+            <div className="bg-purple-100 flex flex-row justify-center font-poppins font-bold text-2xl text-white text-center">
               Algumas funcionalidades do nosso aplicativo
             </div>
             <div className="bg-red-100 flex flex-row justify-center">
-              Aqui vai a imagem da direita
+              <img className="w-4/5" src={imgAppUtilities}></img>
             </div>
             <div className="bg-blue-100 flex flex-row justify-around items-center h-10 ">
-              <div>Botao 1</div>
-              <div>Botao 2</div>
+              <img className="h-11" src={imgAppStore} />
+              <img className="h-11" src={imgPlayStore} />
             </div>
           </div>
         </SectionContent>
