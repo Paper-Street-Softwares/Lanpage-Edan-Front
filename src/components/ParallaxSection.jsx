e4573664a363618fc599ec4e39e8445d53656d12
Edison Matos <edison.matos@live.com>
2024-01-31 18:15:14 -0300

    feat: Finaliza seção serviços

--- src/components/ParallaxSection.jsx
+++ src/components/ParallaxSection.jsx
@@ -2,7 +2,7 @@ import paralaxBg from "../style/assets/images/paralaxe/paralaxeBg1.jpg";
 
 export default function ParallaxSection() {
   return (
-    <div className="full">
+    <div className="full mt-10">
       <div className="full">
         <div
           className="feature h-52 bg-fixed"
