8c951e46bdf8909cd60953796024a97c312290de
Edison Matos <edison.matos@live.com>
2024-02-01 17:47:38 -0300

    feat: Insere ícone do instagram

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,25 +1,69 @@
 import HonrizontalIconAndText from "./SectionComponents/HorizontalIconAndText";
 import SectionContent from "./SectionComponents/SectionContent";
+import imgHome from "../style/assets/icons/footer/homeIcon.png";
+import imgWhatsapp from "../style/assets/icons/footer/whatsappIcon.png";
+import imgCalendar from "../style/assets/icons/footer/calendarIcon.png";
+import imgLogo from "../style/assets/images/Logo.png";
+import imgInstagram from "../style/assets/icons/footer/instagramIcon.png";
 
 export default function FooterSection() {
   return (
-    <div className="full bg-mediumgreen">
+    <div className="full bg-darkgreen h-auto p-8 ">
       <div>
         <div className="content">
           <SectionContent>
-            <div className="bg-red-500 w-64 h-40">
-              <div className="bg-white">Titulo</div>
+            <div className="bg-red-500 w-64 font-poppins text-sm text-white">
+              <div className="bg-red-500 h-12 mb-4">
+                <img className="h-12" src={imgLogo} />
+              </div>
               <div className="bg-blue-700 h-auto">
-                <HonrizontalIconAndText icon="icone" text="texto" />
-                <HonrizontalIconAndText icon="icone" text="texto" />
+                <HonrizontalIconAndText
+                  icon={imgHome}
+                  textLine1="Av. Lótus, 23 - Nelson Costa, Ilhéus - BA, 45656-000"
+                />
+                <HonrizontalIconAndText
+                  icon={imgWhatsapp}
+                  textLine1="(73) 9.9118-7930"
+                />
+                <HonrizontalIconAndText
+                  icon={imgCalendar}
+                  textLine1="Segunda a Quinta, 08h às 18h"
+                  textLine2="Sexta, 08 às 17h"
+                />
               </div>
             </div>
-            <div className="bg-green-500 w-64 h-40">Sobre</div>
-            <div className="bg-blue-500 w-64 h-40">Navegação</div>
+            <div className="bg-green-500 w-64 font-poppins text-sm text-white">
+              <div className="bg-red-500 h-12  text-2xl font-medium mb-4 ">
+                Sobre
+              </div>
+              <div className="bg-blue-700 mb-4">
+                A Edan Contabilidade tem uma equipe de profissionais treinados e
+                capacitados para atender com primazia todas as demandas nas
+                áreas de departamento pessoal, fiscal e contábil.
+              </div>
+              <div>
+                <HonrizontalIconAndText icon={imgInstagram} />
+              </div>
+            </div>
+            <div className="bg-blue-500 w-64 font-poppins text-sm text-white">
+              <div className="bg-red-500 h-12 text-2xl font-medium mb-4">
+                Navegação
+              </div>
+              <div className="bg-blue-700 h-auto flex flex-row justify-between">
+                <div>
+                  <div className="mb-1">Início</div>
+                  <div className="mb-1"> Serviços</div>
+                  <div className="mb-1">Aplicativo</div>
+                  <div className="mb-1">Links Úteis</div>
+                </div>
+                <div>
+                  <div className="mb-1">Sobre nós</div>
+                  <div className="mb-1">Certidões</div>
+                  <div className="mb-1">Depoimentos</div>
+                </div>
+              </div>
+            </div>
           </SectionContent>
-        </div>
-        <div className="bg-lightgreen w-full text-center">
-          Copyrights e Créditos
         </div>
       </div>
     </div>
--- src/components/SectionComponents/HorizontalIconAndText.jsx
+++ src/components/SectionComponents/HorizontalIconAndText.jsx
@@ -1,13 +1,15 @@
 export default function HonrizontalIconAndText(props) {
-  const { icon, text } = props;
+  const { icon, textLine1, textLine2, textLine3 } = props;
 
   return (
     <div className="bg-blue-300 h-auto w-full flex flex-row justify-start">
       <div className="bg-yellow-400 w-full flex flex-row items-center">
         <div className="bg-green-400 w-1/5 flex flex-row justify-start items-center mr-2">
-          {icon}
+          <img className="h-8" src={icon} />
         </div>
-        <div>{text}</div>
+        <div>{textLine1}</div>
+        <div>{textLine2}</div>
+        <div className="bg-blue-100">{textLine3}</div>
       </div>
     </div>
   );
