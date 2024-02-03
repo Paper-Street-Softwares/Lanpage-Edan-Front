88c87b9e7bb98ccf975c9d750e2c6e1513a94f7c
Edison Matos <edison.matos@live.com>
2024-02-03 18:26:51 -0300

    feat: Insere seção do whatsapp com paralaxe

--- src/components/WppSection.jsx
+++ src/components/WppSection.jsx
@@ -5,13 +5,17 @@ import WhatsAppIcon from "../style/assets/icons/WhatsAppIcon.png";
 
 export default function WppSection() {
   return (
-    <div className="bg-red-100 mt-28 content" id="contact">
+    <div
+      className="bg-fixed bg-red-100 mt-28 content"
+      style={{ backgroundImage: `url(${bgParalax})` }}
+      id="contact"
+    >
       <SectionContent>
-        <div className="w-full h-48 bg-blue-300">
-          <div className="flex flex-row items-end justify-between h-48 bg-blue-500 ">
+        <div className="w-full h-52">
+          <div className="flex flex-row items-end justify-between h-52">
             <div className="bg-grenn-200">
-              <div className="flex flex-col justify-around h-48 bg-blue-700">
-                <h1 className="text-3xl text-white bg-green-800 font-poppins">
+              <div className="flex flex-col justify-around h-52">
+                <h1 className="text-3xl text-white font-poppins">
                   Contate-nos através do <strong>Whatsapp</strong>
                 </h1>
                 <button className="bg-green-400 w-72">
@@ -31,7 +35,7 @@ export default function WppSection() {
                 </button>
               </div>
             </div>
-            <div className="bg-yellow-300">
+            <div>
               <img className="h-64" alt="Foto Edilson" src={imgEdilson} />
             </div>
           </div>
