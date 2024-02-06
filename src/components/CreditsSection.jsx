fc5d888c26cef73749feabd40b9670c7310a5839
Edison Matos <edison.matos@live.com>
2024-02-06 18:07:34 -0300

    feat: Ajusta linha de créditos

--- src/components/CreditsSection.jsx
+++ src/components/CreditsSection.jsx
@@ -1,8 +1,10 @@
 export default function CreditsSection() {
   return (
-    <div className="flex flex-col items-center justify-center h-10 text-xs font-light text-white bg-mediumgreen font-poppins">
-      2024 - Edan Contabilidade - Todos os direitos reservados - Desenvolvido
-      por Paper Street Softwares
+    <div className="flex flex-col items-center justify-center w-full font-light text-white text-10px h-14 content bg-mediumgreen font-poppins md:text-xs">
+      <p className="text-center">
+        2024 - Edan Contabilidade - Todos os direitos reservados - Desenvolvido
+        por Paper Street Softwares
+      </p>
     </div>
   );
 }
