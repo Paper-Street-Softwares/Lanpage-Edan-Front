efd1ddea5fb63ea2d176d80b4abe8f4e85e57aa4
Edison Matos <edison.matos@live.com>
2024-02-06 17:00:08 -0300

    feat: Ajusta responsividade card service

--- src/components/AppSection.jsx
+++ src/components/AppSection.jsx
@@ -9,7 +9,7 @@ export default function AppSection() {
     <div className="content">
       <div className="mt-8 mb-8 feature">
         <SectionContent>
-          <div className="flex flex-col justify-around w-128 h-128 lg:w-112 md:w-72">
+          <div className="flex flex-col justify-around w-60 h-128 md:w-72 lg:w-112 xl:w-128">
             <div className="flex flex-col items-center justify-center h-1/5">
               <div className="flex flex-row justify-center mx-2 text-2xl font-bold text-center font-poppins text-mediumgreen md:mx-3">
                 Baixe nosso aplicativo e tenha inúmeras vantagens
@@ -25,7 +25,7 @@ export default function AppSection() {
               </div>
             </div>
           </div>
-          <div className="flex flex-col justify-around bg-mediumgreen w-128 h-128 lg:w-112 md:w-96">
+          <div className="flex flex-col justify-around bg-mediumgreen w-60 h-128 md:w-72 lg:w-112 xl:w-128">
             <div className="flex flex-col items-center justify-center h-1/5">
               <div className="flex flex-row justify-center mx-2 text-2xl font-bold text-center text-white font-poppins md:mx-4">
                 Algumas funcionalidades do nosso aplicativo
