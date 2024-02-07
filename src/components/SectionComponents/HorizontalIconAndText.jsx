e10388f9361046a0988d57505fe35958af76b6a5
Edison Matos <edison.matos@live.com>
2024-02-07 17:26:43 -0300

    feat: Ajuste responsivo do footer

--- src/components/SectionComponents/HorizontalIconAndText.jsx
+++ src/components/SectionComponents/HorizontalIconAndText.jsx
@@ -2,11 +2,11 @@ export default function HonrizontalIconAndText(props) {
   const { img, imgAlt, text, textLine2, textLine3, textLine4 } = props;
 
   return (
-    <div className="flex">
-      <div className="flex flex-col items-center justify-center">
-        <img className="w-6" alt={imgAlt} src={img} />
+    <div className="flex flex-col bg-yellow-700 phone3:flex-row">
+      <div className="flex flex-col items-center justify-center bg-blue-700">
+        <img className="w-6 " alt={imgAlt} src={img} />
       </div>
-      <div className="flex flex-col justify-center w-full ml-4 items-left">
+      <div className="flex flex-col justify-center w-full bg-green-500 phone3:ml-4 items-left">
         <div>{text}</div>
         <div>{textLine2}</div>
         <div>{textLine3}</div>
