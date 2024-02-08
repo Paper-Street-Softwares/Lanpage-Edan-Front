b930082ba43aa0c9e3529c357357d4df3b55a7b1
GabrielAdans <adans.gabriel@gmail.com>
2024-02-08 14:31:54 -0300

    feat: Implementa parte da lógica de animação slideUp

--- src/components/SectionComponents/Navbar.jsx
+++ src/components/SectionComponents/Navbar.jsx
@@ -3,7 +3,7 @@ import { Children } from "react";
 export default function Navbar({ children }) {
   const childrenArray = Children.toArray(children);
   return (
-    <div className="flex w-full h-auto mx-auto my-8 opacity-0 content animate-fade-in">
+    <div className="flex w-full h-auto mx-auto my-10 opacity-0 phone2:my-12 content animate-fade-in">
       <div className="relative flex justify-between w-full">
         {childrenArray}
       </div>
