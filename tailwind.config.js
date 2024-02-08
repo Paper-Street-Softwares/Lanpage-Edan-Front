d96d2fecec32c65a13cc7620c5a1c2e33dcb9e17
GabrielAdans <adans.gabriel@gmail.com>
2024-02-08 09:01:05 -0300

    fix: Posiciona botão 'solicitar contato'

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,12 +32,12 @@ module.exports = {
       },
       keyframes: {
         slideDown: {
-          "0%": { transform: "translateY(-100%)", opacity: "0" },
-          "100%": { transform: "translateY(0%)", opacity: "1" },
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+          "100%": { transform: "translateX(0%)", opacity: "1" },
         },
         slideUp: {
-          "0%": { transform: "translateY(0%)", opacity: "1" },
-          "100%": { transform: "translateY(-100%)", opacity: "0" },
+          "0%": { transform: "translateX(0%)", opacity: "1" },
+          "100%": { transform: "translateX(-100%)", opacity: "0" },
         },
       },
       animation: {
