be150ac4c4bb50f97c0498b3d438e487e39413d3
Edison Matos <edison.matos@live.com>
2024-02-08 09:50:00 -0300

    feat: Finaliza criação de variáveis pra títulos

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,20 +4,32 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#027738", // Primary collor (lighter)
-        secondary: "#09351C", // Dark variation from primary for small details (darker)
-        tertiary: "", // Optional
-        quaternary: "#737373", // Neutral gray for paragraph
-        quinary: "#f6f6f6", // Light primary collor (or gray) for background
+        primary: "#027738", // Com primária (clara)
+        secondary: "#09351C", // Cor primária (escura)
+        tertiary: "", // Opcional
+        quaternary: "#737373", // Cinza neutro -> Parágrafos
+        quinary: "#f6f6f6", // Com primária bem clara ou cinza -> Bg de seções
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
       fontSize: {
         xxs: "0.625rem",
+        title1: "1.25rem", // 20px -> | Cards Mobile e subtítulos de seção
+        title2: "1.375rem", // 22px -> Cards com muita info | Section header mobile, paralaxe
+        title3: "1.5rem", // 24px -> Cards de serviços | Hero mobile
+        title4: "1.875rem", // 30px -> Paralax e Seções 1/2 | Números grandes
+        title5: "2.25rem", // 36px -> Section header
+        title6: "3rem", // 48px -> Números grandes
+        title7: "3.4375rem", // 55px -> Hero desktop
+        paragraph1: "",
+        paragraph2: "",
+        paragraph3: "",
+        paragraph4: "",
       },
       spacing: {
         112: "28rem",
+        120: "30rem",
         128: "32rem",
       },
       screens: {
@@ -44,7 +56,7 @@ module.exports = {
         "slide-down": "slideDown 2s ease",
         "slide-up": "slideUp 2s ease",
       },
-    }
+    },
   },
   plugins: [],
 };
