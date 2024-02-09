9bb077b937808bff6cdf8440102f666961492098
Edison Matos <edison.matos@live.com>
2024-02-09 11:50:31 -0300

    feat: Ajusta responsividade de seção Aplicativo Edan

--- src/components/AppSection.jsx
+++ src/components/AppSection.jsx
@@ -7,39 +7,39 @@ import imgPlayStore from "../style/assets/images/aplicativo/playStore.png";
 export default function AppSection() {
   return (
     <div className="content">
-      <div className="mt-8 mb-8 feature">
+      <div className="mt-8 mb-8 bg-neutral-200 feature">
         <SectionContent>
-          <div className="flex flex-col justify-around w-full bg-yellow-500 h-96 tablet1:h-112 tablet2:w-72 desktop1:w-112 xl:w-128">
-            <div className="flex flex-col items-center justify-center bg-red-700 h-1/5">
-              <div className="flex flex-row justify-center mx-2 text-2xl font-bold text-center bg-green-200 font-poppins text-secondary tablet1:text-title3 tablet2:mx-3">
-                Baixe nosso aplicativo e tenha inúmeras vantagens
-              </div>
+          <div className="flex flex-col justify-between w-full bg-yellow-500 h-128 tablet2:w-72 desktop1:w-112">
+            <div className="flex flex-col items-center justify-center bg-red-700 h-28">
+              <h1 className="flex flex-col justify-center font-bold text-center bg-green-200 text-title1 font-poppins text-secondary">
+                Baixe nosso aplicativo e tenha inúmeras vantagens asdasd dasdasd
+                sdadasd
+              </h1>
             </div>
-            <div className="flex flex-col items-center justify-around h-4/5">
-              <div className="flex flex-row items-center justify-center ">
-                <img
-                  className="h-4/5 tablet1:h-3/5 tablet2:h-4/5"
-                  alt="Telas de celulares"
-                  src={imgAppScreen}
-                />
-              </div>
+            <div className="flex flex-col items-center justify-around flex-grow bg-blue-500">
+              <img
+                className="w-11/12"
+                alt="Telas de celulares"
+                src={imgAppScreen}
+              />
             </div>
           </div>
-          <div className="flex flex-col justify-around w-full bg-secondary h-96 tablet1:h-[680px] phone1:h-112 phone2:h-128 tablet2:w-72 tablet2:h-128 desktop1:w-112">
-            <div className="flex flex-col items-center justify-center bg-pink-500 h-1/5">
-              <div className="flex flex-row justify-center mx-4 mt-6 text-base font-bold text-center text-white bg-blue-700 font-poppins tablet1:text-title3 tablet2:text-2xl tablet2:mx-4">
-                Algumas funcionalidades do nosso aplicativo
-              </div>
+          <div className="flex flex-col justify-start w-full bg-secondary h-128 tablet2:w-72 desktop1:w-112">
+            <div className="flex items-center justify-center bg-pink-500 h-28">
+              <h1 className="flex flex-row justify-center font-bold text-center text-white bg-blue-700 text-title1 font-poppins">
+                Baixe nosso aplicativo e tenha inúmeras vantagens asdasd dasdasd
+                sdadasd
+              </h1>
             </div>
-            <div className="flex flex-col items-center justify-around bg-green-400 h-4/5">
+            <div className="flex flex-col items-center justify-around flex-grow bg-green-600">
               <div className="flex flex-row justify-center bg-red-600">
                 <img
-                  className="bg-blue-800 w-5/6 tablet2:w-[90%]"
+                  className="w-4/5 bg-blue-800"
                   alt="Utilidades do aplicativo Edan"
                   src={imgAppUtilities}
                 ></img>
               </div>
-              <div className="bg-pink-700 flex flex-row justify-around w-4/5 h-8 tablet2:w-[90%]">
+              <div className="flex flex-row justify-between w-4/5 bg-pink-700">
                 <a
                   href="https://apps.apple.com/il/app/edan-contabilidade/id6473044144"
                   target="_blank"
