7255d81a74b1269a7f1ec0cf4776f7942fb9e628
Edison Matos <edison.matos@live.com>
2024-02-09 14:53:26 -0300

    feat: Finaliza responsividade seção app

--- src/components/AppSection.jsx
+++ src/components/AppSection.jsx
@@ -9,23 +9,23 @@ export default function AppSection() {
     <div className="content">
       <div className="mt-8 mb-8 bg-neutral-200 feature">
         <SectionContent>
-          <div className="flex flex-col justify-between w-full bg-yellow-500 h-128 tablet1:w-64 tablet2:w-80 desktop1:w-112 desktop2:w-128">
+          <div className="flex flex-col justify-between w-full bg-yellow-500 tablet1:w-72 tablet2:w-80 desktop1:w-112 desktop2:w-128">
             <div className="flex flex-col items-center justify-center h-24 bg-red-700">
-              <h1 className="flex flex-col justify-center font-bold text-center bg-green-200 text-title1 phone1:text-title2 font-poppins text-secondary">
+              <h1 className="flex flex-col justify-center font-bold text-center bg-green-200 text-title1 phone1:text-title2 tablet1:text-title1 tablet2:text-title2 font-poppins text-secondary">
                 Baixe nosso aplicativo e tenha inúmeras vantagens
               </h1>
             </div>
             <div className="flex flex-col items-center justify-around flex-grow bg-blue-500">
               <img
-                className="w-11/12 tablet1:w-4/5 desktop2:w-4/5"
+                className="w-full desktop1:w-[90%]"
                 alt="Telas de celulares"
                 src={imgAppScreen}
               />
             </div>
           </div>
-          <div className="flex flex-col justify-start w-full bg-secondary h-128 tablet1:w-64 tablet2:w-80 desktop1:w-112 desktop2:w-128">
+          <div className="flex flex-col justify-start w-full bg-secondary tablet1:w-64 tablet2:w-80 desktop1:w-112 desktop2:w-128">
             <div className="flex items-center justify-center h-24 bg-pink-500">
-              <h1 className="flex flex-row justify-center font-bold text-center text-white bg-blue-700 text-title1 desktop1:text-title2 font-poppins">
+              <h1 className="flex flex-row justify-center font-bold text-center text-white bg-blue-700 phone1:text-title2 text-title1 tablet1:text-paragraph5 tablet2:text-title2 desktop1:text-title2 font-poppins">
                 Algumas funcionalidades do nosso aplicativo
               </h1>
             </div>
@@ -37,13 +37,13 @@ export default function AppSection() {
                   src={imgAppUtilities}
                 ></img>
               </div>
-              <div className="flex flex-row justify-between w-4/5 bg-pink-700">
+              <div className="flex flex-row justify-between w-4/5 my-4 bg-pink-700 desktop1:my-8">
                 <a
                   href="https://apps.apple.com/il/app/edan-contabilidade/id6473044144"
                   target="_blank"
                 >
                   <img
-                    className="h-8 tablet1:h-12 tablet2:h-11"
+                    className="w-32"
                     alt="Botão App Store"
                     src={imgAppStore}
                   />
@@ -53,7 +53,7 @@ export default function AppSection() {
                   target="_blank"
                 >
                   <img
-                    className="h-8 tablet1:h-12 tablet2:h-11"
+                    className="w-32"
                     alt="Botão Play Store"
                     src={imgPlayStore}
                   />
