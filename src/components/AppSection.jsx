dc183d0a0d2b57ae3f005656a6a86db7958cca43
Edison Matos <edison.matos@live.com>
2024-02-09 14:24:58 -0300

    feat: Ajusta títulos e larguras

--- src/components/AppSection.jsx
+++ src/components/AppSection.jsx
@@ -9,26 +9,24 @@ export default function AppSection() {
     <div className="content">
       <div className="mt-8 mb-8 bg-neutral-200 feature">
         <SectionContent>
-          <div className="flex flex-col justify-between w-full bg-yellow-500 h-128 tablet2:w-72 desktop1:w-112">
-            <div className="flex flex-col items-center justify-center bg-red-700 h-28">
-              <h1 className="flex flex-col justify-center font-bold text-center bg-green-200 text-title1 font-poppins text-secondary">
-                Baixe nosso aplicativo e tenha inúmeras vantagens asdasd dasdasd
-                sdadasd
+          <div className="flex flex-col justify-between w-full bg-yellow-500 h-128 tablet1:w-64 tablet2:w-80 desktop1:w-112 desktop2:w-128">
+            <div className="flex flex-col items-center justify-center h-24 bg-red-700">
+              <h1 className="flex flex-col justify-center font-bold text-center bg-green-200 text-title1 phone1:text-title2 font-poppins text-secondary">
+                Baixe nosso aplicativo e tenha inúmeras vantagens
               </h1>
             </div>
             <div className="flex flex-col items-center justify-around flex-grow bg-blue-500">
               <img
-                className="w-11/12"
+                className="w-11/12 tablet1:w-4/5 desktop2:w-4/5"
                 alt="Telas de celulares"
                 src={imgAppScreen}
               />
             </div>
           </div>
-          <div className="flex flex-col justify-start w-full bg-secondary h-128 tablet2:w-72 desktop1:w-112">
-            <div className="flex items-center justify-center bg-pink-500 h-28">
-              <h1 className="flex flex-row justify-center font-bold text-center text-white bg-blue-700 text-title1 font-poppins">
-                Baixe nosso aplicativo e tenha inúmeras vantagens asdasd dasdasd
-                sdadasd
+          <div className="flex flex-col justify-start w-full bg-secondary h-128 tablet1:w-64 tablet2:w-80 desktop1:w-112 desktop2:w-128">
+            <div className="flex items-center justify-center h-24 bg-pink-500">
+              <h1 className="flex flex-row justify-center font-bold text-center text-white bg-blue-700 text-title1 desktop1:text-title2 font-poppins">
+                Algumas funcionalidades do nosso aplicativo
               </h1>
             </div>
             <div className="flex flex-col items-center justify-around flex-grow bg-green-600">
--- src/components/SectionComponents/SectionContent.jsx
+++ src/components/SectionComponents/SectionContent.jsx
@@ -3,7 +3,7 @@ import { Children } from "react";
 export default function SectionContent({ children }) {
   const childrenArray = Children.toArray(children);
   return (
-    <div className="flex flex-wrap items-start justify-around w-full h-auto opacity-0 animate-fade-in tablet2:justify-between">
+    <div className="flex flex-wrap items-start justify-around w-full h-auto opacity-0 animate-fade-in tablet1:justify-between">
       {childrenArray}
     </div>
   );
