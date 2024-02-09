d70df6b3fac93651d62692869a4bc8593b33d95f
Edison Matos <edison.matos@live.com>
2024-02-09 19:35:50 -0300

    feat: Ajusta responsividade dos cards de services

--- src/components/SectionComponents/ImgAndTitleCard.jsx
+++ src/components/SectionComponents/ImgAndTitleCard.jsx
@@ -8,7 +8,7 @@ export default function ImgAndTitleCard(props) {
     <a
       href={link}
       target="_blank"
-      className="flex flex-col items-center justify-between w-32 mt-2 mb-2 h-28 phone3:w-[25%] desktop1:w-[25%]"
+      className="flex flex-col items-center justify-between w-32 mt-2 mb-2 h-28 phone3:w-[50%] tablet1:w-[25%]"
       rel="noreferrer"
     >
       <div className="flex flex-row items-end justify-center w-full h-full">
@@ -17,7 +17,7 @@ export default function ImgAndTitleCard(props) {
         </div>
       </div>
       <div className="flex flex-col justify-center w-full h-full">
-        <div className="font-medium text-center transition duration-300 text-paragraph3 phone3:text-paragraph2 tablet1:text-paragraph4 font-poppins text-secondary hover:text-primary">
+        <div className="font-medium text-center transition duration-300 text-paragraph3 tablet1:text-paragraph4 font-poppins text-secondary hover:text-primary">
           {title}
         </div>
       </div>
