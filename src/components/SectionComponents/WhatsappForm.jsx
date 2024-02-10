0804182a17d2ed0840ece976af5aa708a3c6e5b3
GabrielAdans <adans.gabriel@gmail.com>
2024-02-09 22:00:22 -0300

    fix: Minors bugfixes

--- src/components/SectionComponents/WhatsappForm.jsx
+++ src/components/SectionComponents/WhatsappForm.jsx
@@ -51,7 +51,7 @@ const WhatsappForm = () => {
   };
 
   const validateEmail = (email) => {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return emailPattern.test(email);
   };
 
