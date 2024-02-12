9c7543db100be6fde21a713049dfdc2abb7b06b9
Edison Matos <edison.matos@live.com>
2024-02-12 11:14:33 -0300

    feat: Remove bgs para visualizar deploy

--- src/components/SectionComponents/ResultsCard.jsx
+++ src/components/SectionComponents/ResultsCard.jsx
@@ -2,17 +2,15 @@ export default function HonrizontalIconAndText(props) {
   const { img, imgAlt, number, text } = props;
 
   return (
-    <div className="bg-red-900 flex flex-row my-[10%] phone3:my-[3%] w-[85%] phone3:w-[40%] items-center font-poppins phone3:mx-[2%]">
-      <div className="bg-green-900 flex flex-col justify-center items-center h-16 w-16 border-solid border-[3px] border-verylighter">
+    <div className=" flex flex-row my-[10%] phone3:my-[3%] w-[85%] phone3:w-[40%] items-center font-poppins phone3:mx-[2%]">
+      <div className=" flex flex-col justify-center items-center h-16 w-16 border-solid border-[3px] border-verylighter">
         <img className="w-[40%]" alt={imgAlt} src={img} />
       </div>
-      <div className="bg-blue-800 flex flex-col justify-center flex-1 ml-[5%]">
-        <div className="font-extrabold text-left bg-pink-500 text-verylighter text-title4">
+      <div className="flex flex-col justify-center flex-1 ml-[5%]">
+        <div className="font-extrabold text-left text-verylighter text-title4">
           {number}
         </div>
-        <div className="text-left text-white bg-yellow-700 text-paragraph2">
-          {text}
-        </div>
+        <div className="text-left text-white text-paragraph2">{text}</div>
       </div>
     </div>
   );
