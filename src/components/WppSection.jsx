4ad6f3236a1b45847cbe852e2689e92719b5e1f3
Edison Matos <edison.matos@live.com>
2024-02-12 18:00:42 -0300

    feat: Ajusta responsividade de seção de chamada pro whatsapp

--- src/components/WppSection.jsx
+++ src/components/WppSection.jsx
@@ -16,28 +16,32 @@ export default function WppSection() {
     >
       <SectionContent>
         <div className="w-full">
-          <div className="flex flex-row items-end justify-between ">
-            <div className="flex flex-col justify-end h-28">
-              <h1 className="text-center text-white text-paragraph1 font-poppins">
+          <div className="flex flex-row items-end justify-between h-28">
+            <div className="flex flex-col w-full justify-evenly items-left h-28">
+              <h1 className="text-left text-white text-paragraph1 max-w-32 font-poppins">
                 Contate-nos através do <strong>Whatsapp</strong>
               </h1>
-              <div className="flex flex-row items-center justify-center h-14 tablet2:w-72">
+              <div className="flex flex-col justify-center max-w-32 items-left h-14 tablet2:w-72">
                 <a
                   href="#"
-                  className="flex flex-row items-center justify-around w-[95%] h-[80%] transition rounded-lg bg-primary hover:bg-secondary"
+                  className="flex flex-row px-1 justify-around items-center w-full max-w-32 h-[80%] transition rounded-lg bg-primary hover:bg-secondary"
                 >
                   <img
                     src={WhatsAppIcon}
-                    className="w-[30%] mx-[8%]"
+                    className="h-[45%]"
                     alt="WhatsApp Icon"
                   ></img>
-                  <p className="text-white text-paragraph2">
-                    Solicitar contato
+                  <p className="font-bold text-white text-paragraph1">
+                    Solicitar Contato
                   </p>
                 </a>
               </div>
             </div>
-            <img className="w-20 " alt="Foto Edilson" src={imgEdilson} />
+            <img
+              className="w-[45%] max-w-28"
+              alt="Foto Edilson"
+              src={imgEdilson}
+            />
           </div>
         </div>
       </SectionContent>
