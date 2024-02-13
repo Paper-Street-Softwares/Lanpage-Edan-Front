4f34301f91da176c60a5776926a74497fdc52f0d
Edison Matos <edison.matos@live.com>
2024-02-13 08:12:55 -0300

    feat: Conclui responsividade de seção footer

--- src/components/SectionComponents/HorizontalIconAndText.jsx
+++ src/components/SectionComponents/HorizontalIconAndText.jsx
@@ -2,7 +2,7 @@ export default function HonrizontalIconAndText(props) {
   const { img, imgAlt, text, textLine2, textLine3, textLine4 } = props;
 
   return (
-    <div className="flex flex-row my-[2%] tablet2:my-[1%]">
+    <div className="flex flex-row my-[2%] tablet1:my-[3%]">
       <div className="flex flex-col justify-center w-[7%] tablet1:w-[3%] tablet2:w-[7%] ">
         <img className="" alt={imgAlt} src={img} />
       </div>
