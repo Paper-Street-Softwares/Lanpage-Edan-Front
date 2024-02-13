954ec404a04695b8b883a9af5246c1fb9d1cfdf6
Edison Matos <edison.matos@live.com>
2024-02-13 09:29:49 -0300

    feat: Finaliza seção CTA para 475px

--- src/components/WppSection.jsx
+++ src/components/WppSection.jsx
@@ -6,7 +6,7 @@ import WhatsAppIcon from "../style/assets/icons/WhatsAppIcon.png";
 export default function WppSection() {
   return (
     <div
-      className="mt-12 phone1:mt-[15%] bg-center bg-cover tablet2:mt-28 content"
+      className="mt-12 phone1:mt-[15%] phone2:mt-[20%] phone3:mt-[20%] bg-center bg-cover tablet2:mt-28 content"
       style={{
         backgroundImage: `url(${imgOfficeGreenBackground})`,
         backgroundRepeat: "no-repeat",
@@ -16,21 +16,21 @@ export default function WppSection() {
     >
       <SectionContent>
         <div className="w-full">
-          <div className="flex flex-row items-end justify-between h-28 phone1:h-40">
-            <div className="flex flex-col w-full justify-evenly items-left h-28 phone1:h-40">
-              <h1 className="text-left text-white text-paragraph1 phone1:text-paragraph4 max-w-32 font-poppins">
+          <div className="flex flex-row items-end justify-between h-28 phone1:h-40 phone3:h-48">
+            <div className="flex flex-col w-full justify-evenly items-left h-28 phone1:h-40 phone3:h-48">
+              <h1 className="text-left text-white text-paragraph1 phone1:text-paragraph4 phone3:text-paragraph5 max-w-32 font-poppins">
                 Contate-nos através do <strong>Whatsapp</strong>
               </h1>
               <a
                 href="#"
-                className="flex flex-row items-center justify-around w-full h-12 px-1 transition rounded-lg max-w-32 bg-primary hover:bg-secondary"
+                className="flex flex-row items-center justify-around w-[90%] h-8 phone1:h-12 px-1 transition rounded-lg max-w-32 phone3:max-w-none phone3:w-[90%] bg-primary hover:bg-secondary"
               >
                 <img
                   src={WhatsAppIcon}
                   className="h-[45%]"
                   alt="WhatsApp Icon"
                 ></img>
-                <p className="text-white text-paragraph1 phone1:text-paragraph3">
+                <p className="text-white text-paragraph1 phone1:text-paragraph3 phone3:text-paragraph5">
                   Clique aqui
                 </p>
               </a>
