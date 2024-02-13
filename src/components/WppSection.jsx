84bce4690f9e4ca34d45b3f25c7fbd2aca807f82
Edison Matos <edison.matos@live.com>
2024-02-13 10:24:44 -0300

    feat: Finaliza seção cta edan

--- src/components/WppSection.jsx
+++ src/components/WppSection.jsx
@@ -16,27 +16,27 @@ export default function WppSection() {
     >
       <SectionContent>
         <div className="w-full">
-          <div className="flex flex-row items-end justify-between h-28 phone1:h-40 phone3:h-48">
-            <div className="flex flex-col w-full justify-evenly items-left h-28 phone1:h-40 phone3:h-48">
-              <h1 className="text-left text-white text-paragraph1 phone1:text-paragraph4 phone3:text-paragraph5 max-w-32 font-poppins">
+          <div className="flex flex-row items-end justify-between h-28 phone1:h-40 phone3:h-48 desktop1:h-52">
+            <div className="flex flex-col w-full justify-evenly items-left h-28 phone1:h-40 phone3:h-48 desktop1:h-52">
+              <h1 className="text-left text-white text-paragraph1 phone1:text-paragraph4 phone3:text-paragraph5 max-w-32 phone3:max-w-none font-poppins tablet1:text-title2 tablet2:text-title3 desktop1:text-title4">
                 Contate-nos através do <strong>Whatsapp</strong>
               </h1>
               <a
                 href="#"
-                className="flex flex-row items-center justify-around w-[90%] h-8 phone1:h-12 px-1 transition rounded-lg max-w-32 phone3:max-w-none phone3:w-[90%] bg-primary hover:bg-secondary"
+                className="flex flex-row items-center justify-around w-[90%] h-8 phone1:h-12 px-1 transition rounded-lg max-w-32 tablet1:h-[30%] phone3:max-w-48 tablet1:max-w-60 phone3:w-[90%] bg-primary hover:bg-secondary"
               >
                 <img
                   src={WhatsAppIcon}
-                  className="h-[45%]"
+                  className="h-[45%] phone3:h-[60%] tablet1:h-20"
                   alt="WhatsApp Icon"
                 ></img>
-                <p className="text-white text-paragraph1 phone1:text-paragraph3 phone3:text-paragraph5">
+                <p className="text-white text-paragraph1 phone1:text-paragraph3 phone3:text-paragraph5 tablet1:pr-[3%] tablet1:font-bold tablet1:text-title2">
                   Clique aqui
                 </p>
               </a>
             </div>
             <img
-              className="w-[45%] max-w-28 phone1:w-[50%] phone1:max-w-none"
+              className="w-[45%] max-w-28 phone1:w-[50%] phone1:max-w-none phone3:max-w-56 tablet1:w-[35%]"
               alt="Foto Edilson"
               src={imgEdilson}
             />
