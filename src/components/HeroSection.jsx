cbc35fb8c412e372cee3dc391adfd46df81469cc
GabrielAdans <adans.gabriel@gmail.com>
2024-02-15 21:48:25 -0300

    feat: Ajusta responsividade do Botão Wpp, altera tamanho de fontes da HeroSection em dispositívos móveis e dá respiro às divs da HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -11,16 +11,16 @@ export default function HeroSection() {
           className="w-full bg-center bg-cover content md:min-h-0"
           style={{ backgroundImage: "url(" + bgImage + ")" }}
         >
-          <div className="flex flex-wrap items-center justify-between w-full h-full mt-10 text-white tablet1:flex-nowrap font-poppins ">
+          <div className="flex flex-wrap items-center justify-between w-full h-full mt-12 text-white tablet1:flex-nowrap font-poppins ">
             <div className="w-full tablet2:flex-1 tablet2:mr-11">
               <div className="mt-16 tablet1:h-96">
-                <h1 className="mb-2 font-bold leading-7 text-title4 tablet1:leading-none tablet1:text-title5 tablet2:text-title6">
+                <h1 className="mb-2 font-bold leading-7 text-title4 phone3:text-title7 phone3:leading-[60px] tablet1:leading-none tablet1:text-title6 tablet2:text-title6">
                   <MotionDivLeftToRight>Solução e</MotionDivLeftToRight>
                 </h1>
-                <h1 className="font-bold leading-7 mb-9 text-title4 tablet1:leading-none tablet1:text-title5 tablet2:text-title6 phone2:mb-11">
+                <h1 className="font-bold leading-7 mb-9 text-title4 phone3:text-title7 phone3:leading-[60px] tablet1:leading-none tablet1:text-title6 tablet2:text-title6">
                   <MotionDivLeftToRight>Resultado!</MotionDivLeftToRight>
                 </h1>
-                <p className="text-paragraph3 tablet2:text-paragraph4">
+                <p className="text-paragraph3 tablet2:text-paragraph4 phone3:text-title1 tablet1:text-paragraph3 tablet1:mr-4">
                   <MotionDivLeftToRight>
                     Experiência que Conta! Com mais de 40 anos de experiência,
                     oferecemos conhecimento sólido para lidar com os desafios
@@ -29,7 +29,7 @@ export default function HeroSection() {
                 </p>
               </div>
             </div>
-            <div className="w-full mb-16 tablet2:w-auto phone1:mt-0">
+            <div className="w-full mb-16 tablet2:w-auto phone1:mt-0 tablet1:ml-[8%]">
               <MotionDivRightToLeft>
                 <WhatsappForm />
               </MotionDivRightToLeft>
