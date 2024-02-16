76b9db7a27c94477e62d8e30ed532fdfd95622f3
GabrielAdans <adans.gabriel@gmail.com>
2024-02-16 17:42:49 -0300

    feat: Configura tamanho de texto para resoluções menores que 320px para o botão solicitar contato no wppform

--- src/components/SectionComponents/WhatsappForm.jsx
+++ src/components/SectionComponents/WhatsappForm.jsx
@@ -200,7 +200,7 @@ const WhatsappForm = () => {
         >
           <div className="grid grid-cols-2 items-center w-[70%]">
             <img src={WhatsAppIcon} className="w-16 h-16" alt="WhatsApp Icon" />
-            <p className="whitespace-nowrap phone2:text-title2 tablet1:text-title1">
+            <p className="whitespace-nowrap text-paragraph4 phone1:text-paragraph5 phone2:text-title2 tablet1:text-title1">
               Solicite contato
             </p>
           </div>
